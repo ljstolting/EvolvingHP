b2adea3b5cf1ea44725d7dd98d717d24eff537ea
ljstolting <ljstolting@gmail.com>
2024-11-30 17:34:49 -0500

    long generalist evol

--- Cpp/lindsayevols.sh
+++ Cpp/lindsayevols.sh
@@ -6,20 +6,21 @@
 #SBATCH -e lindsay_%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
-#SBATCH --time=6:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=16G
 #SBATCH -A r00213
 
-# mkdir ./Generalist\ HP\ Mechanisms/$JB;
-cd ./Specifically\ Evolved\ HP\ mechanisms/Every\ Circuit/$JB;
-# echo "ND" $JB
-for ((n = 1; n < 5; n += 1));
-do
-    mkdir ./$n;
-    cd ./$n;
-    echo "ND" $JB;
-    time ../../../../main $JB;
-    cd ../;
-done
-cd ../../../../;
+mkdir ./Generalist\ HP\ Mechanisms/$JB;
+cd ./Generalist\ HP\ Mechanisms/$JB; #cd ./Specifically\ Evolved\ HP\ mechanisms/Every\ Circuit/$JB;
+echo "ND" $JB
+# for ((n = 14; n < 15; n += 1));
+# do
+#     mkdir ./$n;
+#     cd ./$n;
+#     echo "ND" $JB;
+#     time ../../../../main $JB;
+#     cd ../;
+# done
+time ../../main $JB
+cd ../../; #cd ../../../../;
 
