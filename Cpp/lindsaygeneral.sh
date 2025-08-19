428b17130e06540ee0b5bde75b3b5b7389a4e562
ljstolting <ljstolting@gmail.com>
2025-08-18 20:17:27 -0400

    the actual program files might contain changes

--- Cpp/lindsaygeneral.sh
+++ Cpp/lindsaygeneral.sh
@@ -6,14 +6,17 @@
 #SBATCH -e lindsay_%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
-#SBATCH --time=19:30:00
+#SBATCH --time=20:00:00
 #SBATCH --mem=16G
 #SBATCH -A r00213
 
-cd ./Specifically\ Evolved\ HP\ mechanisms/Every\ Circuit/$JB;
+# cd ./Specifically\ Evolved\ HP\ mechanisms/Every\ Circuit/$JB;
 # cd ./No\ Timing\ Requirements/$JB;
-../../../mainHPparspace $JB;
-cd ../../../;
-# ./main;
+# ../../mainHPparspace $JB;
+# cd ../../;
+
+mkdir ./Test3DHPonPyloricSolutions/predictedHPperformanceslice/$JB;
+cd ./Test3DHPonPyloricSolutions/predictedHPperformanceslice/$JB
+../../../mainavgpyloriccombo $JB;
 
 
