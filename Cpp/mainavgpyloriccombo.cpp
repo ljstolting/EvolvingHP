6060b69ddbfa17101d66e34fd15906cc1414bce8
Lindsay Stolting <ljstolting@gmail.com>
2025-08-23 22:22:07 -0400

    larger tolerance to prevent abberant dismissal of pylorics

--- Cpp/mainavgpyloriccombo.cpp
+++ Cpp/mainavgpyloriccombo.cpp
@@ -4,6 +4,12 @@
 //  There is now a new mode that calculates this online instead of collecting all the
 //  data and feeding it to a Jupyter notebook. 
 
+//Update 8/20/25: This less data-intense way of calculating is getting things closer
+//  (even though in principle it should have no effect...) but the predicted performances 
+//  in 3D are still not agreeing with simulation. To debug, I am adding a mode that identifies
+//  a point of interest, runs averages on the whole volume, but only collects data about 
+//  configurations that would average into that point. 
+
 #include "TSearch.h"
 #include "CTRNN.h"
 #include "random.h"
@@ -14,28 +20,34 @@ const double slice_step = 0.1;
 
 // Task params
 const double TransientDuration = 50; //seconds without HP 
-const double RunDuration = 50; //seconds to look for an oscillation cycle
-const double leaving_tolerance = 0.05; // in state space for greater accuracy
-const double return_tolerance = 0.025; //less than leaving tolerance
-int max_steps = int((RunDuration*3)/StepSize); //how much memory to allocate for the max size outputhistory
+const double RunDuration = 10; //max seconds to look for a single oscillation cycle
+const double leaving_tolerance = 3*StepSize; // in state space for greater accuracy
+const double return_tolerance = 2*StepSize; //less than leaving tolerance
+int max_steps = int((RunDuration*4)/StepSize); //how much memory to allocate for the max size outputhistory
 
 const int N = 3;
 
 // new mode for keeping track of the files 
+bool record_all = false;     //previously, we recorded all the averages and their pyloric fitness
+                             //for later data analysis (=true), but we can turn this off (=false)
+// new mode for debugging, keep track only of the calculated circuits that are relevant to a given point in meta-parameter space
+bool audit_mode = true; 
 
-// Input files
+// Input files:
+
+//Local
 // char resfname[] = "./Test3DHPonPyloricSolutions/res_high_test.dat";  
 // char circuitfname[] = "./Specifically Evolved HP mechanisms/Every Circuit/39/pyloriccircuit.ns";
 // char dimsfname[] = "./avgsdimensions.dat";
-//Supercomputer
+// char metaparresfname[] = "./metaparres.dat";
+
+//Supercomputer (from within folder)
 char resfname[] = "../../../Test3DHPonPyloricSolutions/res_high.dat";  
 char circuitfname[] = "../../../Specifically Evolved HP mechanisms/Every Circuit/39/pyloriccircuit.ns";
 char dimsfname[] = "../../../avgsdimensions.dat";
-
-bool record_all = false;     //previously, we recorded all the averages and their pyloric fitness
-                             //for later data analysis (=true), but we can turn this off (=false)
 char metaparresfname[] = "../../../metaparres.dat"; //and instead only record the predicted status of the points in HP metapar space
                                              //the resolution of the evaluated points should be the same as the simulated res
+char ptofinterestfname[] = "./pt_of_interest.dat";
 
 // Output files
 // LEGACY MODE: track average at each point
@@ -43,15 +55,20 @@ char metaparresfname[] = "../../../metaparres.dat"; //and instead only record th
 // LEGACY MODE: track pyloric fitness at each point
 // char out2fname[] = "./Test3DHPonPyloricSolutions/pyloricslice3D_15slice1.dat"; //changes for each compilation
 
-// NEW MODE: track the predicted HP status at each point in HP metapar space
+// ONLINE PROCESSING MODE: track the predicted HP status at each point in HP metapar space
 //  will return two matrices of integers for each point in lattice representing numbers of pyloric
 // char out1fname[] = "./Test3DHPonPyloricSolutions/predictedADHPstatus_pyloric_39.dat";
 // //  and nonpyloric circuits around that average
 // char out2fname[] = "./Test3DHPonPyloricSolutions/predictedADHPstatus_nonpyloric_39.dat"; 
 
 //Supercomputer
-char out1fname[] = "./predictedADHPstatus_pyloric_39.dat";
-char out2fname[] = "./predictedADHPstatus_nonpyloric_39.dat"; 
+// char out1fname[] = "./predictedADHPstatus_pyloric_39.dat";
+// char out2fname[] = "./predictedADHPstatus_nonpyloric_39.dat"; 
+
+// AUDIT MODE (supercomputer):
+char out1fname[] = "./relevant_pt_data.dat";           //biases, exact avgs, pyloric fitness, pyloric features, leftward stability
+char out2fname[] = "./relevant_pt_trajectories.dat";   //trajectories over RunDuration
+double avgs_track[3201][3201][N]; //zero-indexed matrix to keep running track of averages for stability
 
 int main (int argc, const char* argv[]) 
 {
@@ -81,6 +98,13 @@ int main (int argc, const char* argv[])
         exit(EXIT_FAILURE);
     }
 
+    ifstream ptofinterestfile;
+    ptofinterestfile.open(ptofinterestfname);
+    if (!ptofinterestfile) {
+        cerr << "File not found: " << ptofinterestfname << endl;
+        exit(EXIT_FAILURE);
+    }
+
 	// Create files to hold data
 	ofstream outfile1;
 	outfile1.open(out1fname);
@@ -103,7 +127,7 @@ int main (int argc, const char* argv[])
 		}
 	}
 
-    //no longer actually takes first dim from file but takes from the executable call for parallelism
+    //no longer actually takes first dim from file but takes from the executable call (if present) for parallelism
     if (argc>1){
         int slicenum = atoi(argv[1]);
         resmat(1,1) = resmat(1,1)+(slicenum*slice_step); //zero-indexing
@@ -131,11 +155,28 @@ int main (int argc, const char* argv[])
     }
 
     TVector<int> metaparres_lengths(1,regulated_neurons);
-    for (int i=1; i<=num_dims; i++){
+    for (int i=1; i<=regulated_neurons; i++){
         int idxlen = (metaparresmat(i,2) - metaparresmat(i,1))/metaparresmat(i,3);
         idxlen = idxlen+1; //upper bound is inclusive
         metaparres_lengths(i) = idxlen;
     }
+
+    TVector<int> parres_lengths(1,num_dims);
+    for (int i=1; i<=num_dims; i++){
+        int idxlen = (resmat(i,2) - resmat(i,1))/resmat(i,3);
+        idxlen = idxlen+1; //upper bound is inclusive? might not be based on how the indexing works
+        parres_lengths(i) = idxlen;
+    }
+
+    //load in (and round if necessary) the metaparres pt of interest and then convert to metaparres matrix indecies
+    TVector<int> ptofinterest_idx(1,regulated_neurons);
+        for (int i=1; i<=regulated_neurons; i++){
+            double v;
+            ptofinterestfile >> v;
+            v = v-metaparresmat(i,1);
+            int idx = round(v/metaparresmat(i,3));
+            ptofinterest_idx(i) = idx;
+        }
 
     //right now, not general to arbitrary dimensions because I don't know enough about the C++ arrays
     int pyloriccount[metaparres_lengths(1)][metaparres_lengths(2)][metaparres_lengths(3)]; 
@@ -149,11 +190,14 @@ int main (int argc, const char* argv[])
             }
         }
     }
-    // cout << "arrays filled" << endl;
 
     // Set some variables that will be used in the loop
     TVector<double> acc(1,N); //vector to store the value of the proxy expression for each neuron
     bool finished = false;
+    //and initialize all the indecies at 0
+    TVector<int> par_indecies(1,num_dims);
+    par_indecies.FillContents(0);
+    TVector<int> left_stability(1,N);
 
     // And finally, start the calculations
 	while (!finished){
@@ -171,10 +215,8 @@ int main (int argc, const char* argv[])
         for (double t=StepSize;t<=TransientDuration;t+=StepSize){
             Circuit.EulerStep(StepSize,0);
         }
-        // then run to keep track of the neuron values either over three full cycles (to ensure three PD starts)
-        int cycle_count = 0;
+        // new time-saving and consolidating structure: one cycle, calculate average, if average is of interest, then roughly three more cycles, calc pyloric fitness
         int stepnum = 0;
-        int first_cycle_stepnum = 0;
         TVector<double> startstate(1,N);
         TVector<double> avg(1,N);
         avg.FillContents(0.0);
@@ -182,112 +224,140 @@ int main (int argc, const char* argv[])
         double t = StepSize; 
         double dist = 0;
         bool left,returned;
-        while ((cycle_count<3) && ((t<=RunDuration) || (cycle_count>0)) && (stepnum<max_steps)){
-            for (int i = 1; i <= N; i++){
-                startstate[i] = Circuit.NeuronState(i);
-            }
-            // cout << startstate << endl;
-            // allow the neural outputs to leave and come back to the same point
-            left = false;
-            returned = false;
-            while ((!returned) && ((t<=RunDuration) || (cycle_count>0)) && (stepnum<max_steps)){
-                Circuit.EulerStep(StepSize,0);
-                stepnum ++;
-                t += StepSize;
-                dist = 0;
-                for (int i = 1; i <= N; i++){
-                    dist += pow(Circuit.NeuronState(i)-startstate[i],2);
-                    if (cycle_count == 0){                  //only add up the first cycle
-                        avg[i] += Circuit.NeuronOutput(i); 
-                    }
-                    //record in output hist for pyloric evaluation
-                    outputhist(stepnum,i) = Circuit.NeuronOutput(i);
-                }
-                if (cycle_count == 0){first_cycle_stepnum ++;}
-
-                dist = sqrt(dist);
-                // cout << dist << endl;
-
-                if ((dist > leaving_tolerance) && (!left)){
-                    left = true;
-                }
-
-                if (left && (dist < return_tolerance)){
-                    returned = true;
-                    left = false;
-                    cycle_count ++;
-                    // cout << "cycle " << cycle_count << " complete at " << t << endl;
-                    // break;
-                }
-            }
+        left = false;
+        returned = false;
+        for (int i = 1; i <= N; i++){
+            startstate[i] = Circuit.NeuronState(i);
         }
-        int three_cycle_steps = stepnum;
-        while (stepnum < min(three_cycle_steps+10,max_steps)){
-            //run for 10 more steps just in case we started right on a PD crossing
-            stepnum ++;
+        while ((t<=RunDuration) && (returned == false)){
             Circuit.EulerStep(StepSize,0);
+            stepnum ++;
+            t += StepSize;
+            dist = 0;
             for (int i = 1; i <= N; i++){
+                dist += pow(Circuit.NeuronState(i)-startstate[i],2);
+                avg[i] += Circuit.NeuronOutput(i); 
+                //record in output hist
                 outputhist(stepnum,i) = Circuit.NeuronOutput(i);
             }
-        }
-        //take only the part of the output hist that actually ended up getting filled
-        TMatrix<double> newoutputhist(1,stepnum,1,N);
-        for (int i=1;i<=stepnum;i++){
-            for (int j=1;j<=N;j++){
-                newoutputhist(i,j) = outputhist(i,j);
+            dist = sqrt(dist);
+            // cout << dist << endl;
+
+            if ((dist > leaving_tolerance) && (!left)){
+                left = true;
+            }
+
+            if (left && (dist < return_tolerance)){
+                returned = true;
+                left = false;
             }
         }
-        double pyl_fitness;
-        if (cycle_count > 0){
-            // cout << cycle_count << " cycles" << endl;
-            TVector<double> featuresvec(1,8);
-            BurstTimesfromOutputHist(newoutputhist,featuresvec);
-            pyl_fitness = PyloricFitFromFeatures(featuresvec);
+        //calculate average
+        for (int i = 1;i <= N;i++){
+            avg[i] = avg[i]/stepnum;
         }
-        else{
-            pyl_fitness = 0;
-        }
-
-        for (int i = 1; i <= N; i++){
-            // make into true average by dividing by the number of steps in one cycle
-            avg[i] /= first_cycle_stepnum;
-        }
-
-        // LEGACY MODE: record all data into files
-        if (record_all){
-            outfile1 << avg << endl;
-            outfile2 << pyl_fitness << endl;
-        }
-        // NEW MODE: record the number of pyloric and nonpyloric hp's with average values closest to each point
-        else{
+        //calculate the metapar res index if needed
+        TVector<int> add_idx(1,regulated_neurons);
+        add_idx.FillContents(1);
+        if(audit_mode || !record_all){
             //derive the index of the matrix that corresponds with the point's average values
-            TVector<int> add_idx(1,regulated_neurons);
             for (int i=1; i<=regulated_neurons; i++){
                 double v = avg(i)-metaparresmat(i,1);
                 int idx = round(v/metaparresmat(i,3));
                 add_idx(i) = idx;
             }
-            if (pyl_fitness>=.3){
-                pyloriccount[add_idx(1)][add_idx(2)][add_idx(3)] ++;
-            }
-            else{
-                nonpyloriccount[add_idx(1)][add_idx(2)][add_idx(3)] ++;
+        }
+        bool cont = true;
+        if(audit_mode){
+            for (int i=1; i<=regulated_neurons; i++){
+                if (add_idx(i) != ptofinterest_idx(i)){
+                    cont = false;
+                }
             }
         }
-        // cout << parvec << endl;
-        //and then increase the value of the appropriate parameters
+        if(cont){
+            outfile1 << Circuit.biases << " " << avg << " ";
+            //if oscillitory
+            if (returned){
+                int first_cycle_stepnum = stepnum;
+                for (int i=1;i<=3*first_cycle_stepnum;i++){
+                    //run for three more cycles, to ensure 3 PD starts
+                    stepnum ++;
+                    Circuit.EulerStep(StepSize,0);
+                    for (int i = 1; i <= N; i++){
+                        outputhist(stepnum,i) = Circuit.NeuronOutput(i);
+                    }
+                    // if (audit_mode){
+                    //     outfile2 << Circuit.outputs << endl;
+                    // }
+                }
+            }
+        
+            //take only the part of the output hist that actually ended up getting filled
+            TMatrix<double> newoutputhist(1,stepnum,1,N);
+            for (int i=1;i<=stepnum;i++){
+                for (int j=1;j<=N;j++){
+                    newoutputhist(i,j) = outputhist(i,j);
+                }
+            }
+
+            double pyl_fitness =0;
+            TVector<double> featuresvec(1,8);
+            featuresvec.FillContents(0);
+            featuresvec(8) = t;
+            if (returned){
+                // cout << cycle_count << " cycles" << endl;
+                BurstTimesfromOutputHist(newoutputhist,featuresvec);
+                pyl_fitness = PyloricFitFromFeatures(featuresvec);
+            }
+            // LEGACY MODE: record all data into files
+            if (record_all){
+                outfile1 << avg << endl;
+                outfile2 << pyl_fitness << endl;
+            }
+            // ONLINE PROCESSING MODE: record the number of pyloric and nonpyloric hp's with average values closest to each point
+            else{
+                if (audit_mode){
+                    //determine leftward stability if you can (assumes 3 neurons and all relevant for stability)
+                    left_stability.FillContents(0);
+                    if (par_indecies(1) == 0 || (avgs_track[par_indecies(2)][par_indecies(3)][0] < avg[1])){left_stability[1] = 1;}
+                    if (par_indecies(2) == 0 || (avgs_track[par_indecies(2)-1][par_indecies(3)][1] < avg[2])){left_stability[2] = 1;}
+                    if (par_indecies(3) == 0 || (avgs_track[par_indecies(2)][par_indecies(3)-1][2] < avg[3])){left_stability[3] = 1;}
+            
+                    outfile1 << pyl_fitness << " " << featuresvec << " " << left_stability << endl;
+                }
+                else{
+                    if (pyl_fitness>=.3){
+                        pyloriccount[add_idx(1)][add_idx(2)][add_idx(3)] ++;
+                    }
+                    else{
+                        nonpyloriccount[add_idx(1)][add_idx(2)][add_idx(3)] ++;
+                    }
+                }
+            }
+        }
+
+        //and finally overwrite the history matrix used for determining stability 
+        for (int i=1;i<=N;i++){
+            avgs_track[par_indecies(2)][par_indecies(3)][i-1] = avg[i];
+        }
+
 		parvec(num_dims)+=resmat(num_dims,3); //step the last dimension
+        par_indecies(num_dims) ++;
 		for (int i=(num_dims-1); i>=1; i-=1){ //start at the second to last dimension and count backwards to see if the next dimension has completed a run
 			if(parvec(i+1)>resmat(i+1,2)){   //if the next dimension is over its max
 				parvec(i+1) = resmat(i+1,1); //set it to its min
 				parvec(i) += resmat(i,3);    //and step the current dimension
+                par_indecies(i) ++;
+                par_indecies(i+1) = 0;
                 cout << parvec << endl;
+                // cout << par_indecies << endl;
 			}
 		}
 		if (parvec(1)>resmat(1,2)){
 			finished = true;
-            //NEW MODE
-            if (!record_all){
+            //OUTPUTS
+            if (!record_all && !audit_mode){
                 for (int i=0;i<metaparres_lengths(1);i++){
                     for (int j=0;j<metaparres_lengths(2);j++){
                         for (int k=0;k<metaparres_lengths(3);k++){
@@ -307,6 +377,9 @@ int main (int argc, const char* argv[])
     resfile.close();
     dimsfile.close();
     circuitfile.close();
+    metaparresfile.close();
+    ptofinterestfile.close();
+
     outfile1.close();
     outfile2.close();
 
